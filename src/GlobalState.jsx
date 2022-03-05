95833065b2325ecf882dfe5a029db620946e54b2
Jalome <chjalome@gmail.com>
2022-03-05 21:14:39 +0200

    started working on the readme file

--- src/GlobalState.jsx
+++ src/GlobalState.jsx
@@ -3,6 +3,7 @@ import GetArticles from './api/GetArticles';
 // create global state using React Context that will pass API data to chilren components and screens
 export const GlobalState = createContext();
 export const APIDataProvider = ({children}) => {
+    // fetch the articles from the API
     const articles = GetArticles();
     return(
         <GlobalState.Provider value={articles}>
--- src/api/GetArticles.jsx
+++ src/api/GetArticles.jsx
@@ -1,17 +1,16 @@
 import {useState, useEffect} from 'react';
 const GetArticles = ()=>{
     const [articles, setArticles] = useState([]);
-    const [callback, setCallback] = useState(false)
+    const [callback, setCallback] = useState(false);
     useEffect(()=>{
         const getData = ()=>{
             fetch('https://arena-django-backend.herokuapp.com/')
             .then(res=>res.json())
             .then(data=>{
                 setArticles(data);
-                console.log(data);
             })
         }
-        // fetch articles from API
+        // fetch articles from django API
         getData();
 
     },[callback]);
